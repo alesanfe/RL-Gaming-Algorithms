1046e5814c60ed4a374a6fbe8fc8b9ffead8870c
alesanfe <alex.0002002@gmail.com>
2023-05-30 13:57:45 -0700

    feat(update): Frozen Lake #1
    Probando testeo.

--- src/main/python/games/FrozenLake.py
+++ src/main/python/games/FrozenLake.py
@@ -8,8 +8,12 @@ from src.main.python.games.Game import Game
 
 class FrozenLake(Game):
 
-    def __init__(self, environment='FrozenLake-v0', discount_factor=0.9, learning_factor=0.1, iterations=1000):
-        super().__init__(gym.make(environment), discount_factor, learning_factor, iterations)
+    def __init__(self, environment='FrozenLake-v1', discount_factor=0.9, learning_factor=0.1, iterations=1000):
+        super().__init__(gym.make(environment, render_mode='human'), discount_factor, learning_factor, iterations)
+
+    # Resolución del entorno Frozen Lake utilizando Montecarlo con inicios exploratorios
+    def resolve_frozen_lake_by_montecarlo(self):
+        return self.resolve_by_montecarlo()
 
     # Resolución del entorno Frozen Lake utilizando Q-Learning
     def resolve_frozen_lake_by_q_learning(self, epsilon=0.1):
